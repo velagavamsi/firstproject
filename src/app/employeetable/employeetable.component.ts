6d10a460fb6e77cc86a8a96b1c3b24ff51a63240
vamsi <vamsi.velaga@xsilica.com>
2018-01-30 17:59:42 +0530

    project added

---
+++ src/app/employeetable/employeetable.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employeetable',
+  templateUrl: './employeetable.component.html',
+  styleUrls: ['./employeetable.component.css']
+})
+export class EmployeetableComponent  {
+  employee: any[] =[];
+
+  }
